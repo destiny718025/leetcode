088f301f5533633074ea622181816cb12565d9d0
tang <tang@fuba.tech>
2024-01-02 21:27:12 +0800

    13. Roman to Integer

---
+++ python/RomantoInteger.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        num = 0
+        prev = 1000
+
+        for i in range(len(s)):
+            if prev < dict[s[i]]:
+                num -= 2 * prev
+            num += dict[s[i]]
+            prev = dict[s[i]]
+
+        return num
